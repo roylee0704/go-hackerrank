7457f7a155119e272ee5a1e16e43e47c7e239f04
roylee0704 <roylee0704@gmail.com>
2017-05-22 15:05:52 +0800

    solved w bad time complexity

--- others/visiting_manhattan.go
+++ others/visiting_manhattan.go
@@ -3,25 +3,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	// var x, y, l, h int
-	//
-	// fmt.Fscanf(os.Stdin, "%d %d %d %d", &x, &y, &l, &h)
-	//
-	// fmt.Print(x, y, l, h)
+	var width, height, l, h, x, y int
 
-	h1 := hotel{4, 4}
-	h2 := hotel{2, 1}
+	fmt.Fscanf(os.Stdin, "%d %d %d %d", &width, &height, &l, &h)
 
-	l1 := landmark{1, 1}
-	l2 := landmark{2, 2}
+	landmarks := make([]landmark, l)
+	for i := 0; i < l; i++ {
+		fmt.Fscanf(os.Stdin, "%d %d", &x, &y)
+		landmarks[i] = landmark{x, y}
+	}
+
+	hotels := make([]hotel, h)
+	for i := 0; i < h; i++ {
+		fmt.Fscanf(os.Stdin, "%d %d", &x, &y)
+		hotels[i] = hotel{x, y}
+	}
 
 	k := manhanttan{
-		hotels:    []hotel{h1, h2},
-		landmarks: []landmark{l1, l2},
+		hotels:    hotels,
+		landmarks: landmarks,
 	}
 	fmt.Println(k.bestHotel())
 }
@@ -34,7 +39,7 @@ type manhanttan struct {
 func (m manhanttan) bestHotel() int {
 
 	var res, dist int
-	var min = 1000000 // quick hack
+	var min = 10000000 // quick hack
 
 	for i, h := range m.hotels {
 		dist = h.dist(m.landmarks)
